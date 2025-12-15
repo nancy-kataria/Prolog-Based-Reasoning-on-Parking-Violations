3c20c373996ec51567e1ef066ab40e57fb33454f
nancy-kataria <katarianancy8@gmail.com>
2025-12-14 18:15:48 -0800

    remove query files

--- faculty_parking/faculty_parking_query.pl
+++
@@ -1,2 +0,0 @@
-?- reparking_violation(f1_stall1, f1, 45, true).
-?- allowed_to_park(e7, X)
--- student_parking/student_parking_query.pl
+++
@@ -1,4 +0,0 @@
-?- student_parking_violation(s8, 10, true, false,regular_space, none, none, none,none, false, none, false,Reason).
-?- student_parking_violation(e2, 15, true, false,regular_space, none, none, none,none, false, none, false,Reason).
-?- student_parking_violation(s8, 11, true, false,regular_space, none, none, none,blue, true, none, false,Reason).
-?- student_parking_violation(s8, 12, true, false,sidewalk, 10, 20, 10,none, false, none, false,Reason).
